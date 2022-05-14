276605a3204c9e2715e26514fe77adb123855c8e
charles okunzo <okunzopeter01@gmail.com>
2022-05-14 10:05:03 +0300

    feature: add project configurations

---
+++ config.py
@@ -0,0 +1,23 @@
+
+
+
+class Config:
+  pass
+
+
+class ProdConfig(Config):
+  pass
+
+
+
+
+class DevConfig(Config):
+  DEBUG=True
+
+
+
+
+config_options = {
+  'production': ProdConfig,
+  'development': DevConfig
+}
