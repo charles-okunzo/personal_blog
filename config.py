8822c79033484d64c82826432205b59322ab343a
charles okunzo <okunzopeter01@gmail.com>
2022-05-15 18:17:09 +0300

    feature --configure secret key

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
   RANDOM_QUOTE_URL='http://quotes.stormconsultancy.co.uk/random.json'
   SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL')
   SQLALCHEMY_TRACK_MODIFICATIONS=False
+  SECRET_KEY=os.getenv('SECRET_KEY')
 
 
 class ProdConfig(Config):
