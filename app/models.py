f93bd7ad1462680d36c8c0e22f12e3ad0e5f4d42
charles okunzo <okunzopeter01@gmail.com>
2022-05-15 15:12:23 +0300

    feature -- add comments model class

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-from email.policy import default
-from . import db
+from . import db,login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -13,7 +12,9 @@ class RandQuotes:
 
 
 
-
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
 class User(db.Model):
   __tablename__ = 'users'
 
@@ -22,7 +23,7 @@ class User(db.Model):
   email = db.Column(db.String(128), nullable=False, unique=True)
   pass_code = db.Column(db.String(128), nullable=False, unique=True)
   blogposts = db.relationship('BlogPost', backref='user', lazy='dynamic')
-
+  comments = db.relationship('Comment', backref='user', lazy='dynamic')
 
   @property
   def password(self):
@@ -56,6 +57,20 @@ class BlogPost(db.Model):
 
   def __repr__(self):
       return f'User {self.post}'
+
+
+class Comment(db.Model):
+
+  __tablename__ = 'comments'
+
+  id = db.Column(db.Integer, primary_key=True)
+  comment = db.Column(db.String)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
+
+  def __repr__(self):
+      return f'User {self.comment}'
 
 
 
