62298389b11258b0c18da68d7bbe1ec84933319f
charles okunzo <okunzopeter01@gmail.com>
2022-05-15 14:35:12 +0300

    feature--include a verify password function

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class RandQuotes:
@@ -20,8 +21,23 @@ class User(db.Model):
   pass_code = db.Column(db.String(128), nullable=False, unique=True)
 
 
+  @property
+  def password(self):
+    raise AttributeError('You cannot access this password')
+
+
+  @password.setter
+  def password(self, password):
+    self.pass_code = generate_password_hash(password)
+
+
+  def verify_password(self, password):
+    return check_password_hash(self.pass_code, password)
+
+
   def __repr__(self):
       return f'User {self.username}'
+
 
 
   
