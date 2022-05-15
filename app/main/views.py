d6cc7e0d9b42abd318950a5919c6153be5236fe9
charles okunzo <okunzopeter01@gmail.com>
2022-05-15 22:16:06 +0300

    features --add new post form

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,8 @@
+from unicodedata import category
 from flask_login import login_required
+from app import db
+from app.main.forms import BlogForm
+from app.models import BlogPost
 from . import main
 from flask import render_template, url_for, request, redirect
 from ..requests import get_random_quotes
@@ -16,4 +20,17 @@ def index():
 @main.route('/new-post')
 @login_required
 def new_post():
-  ...
+  title = 'PersonalBlog | Create a new Blog post.'
+  form = BlogForm()
+  if form.validate_on_submit():
+    title=form.title.data
+    category=form.category.data
+    post=form.post.data
+
+    new_post_obj=BlogPost(title=title, category=category, post=post)
+    db.session.add(new_post_obj)
+    db.session.commit()
+    
+    return redirect(url_for('main.blogs_page'))
+
+  return render_template('new_post_form.html')
