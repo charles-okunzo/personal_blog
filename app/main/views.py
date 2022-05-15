5c4f22406258b252e680c0ad588bdad93aacbe21
charles okunzo <okunzopeter01@gmail.com>
2022-05-15 12:28:24 +0300

    feature --enabled randomquotedisplay on homepage

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,11 @@
 from . import main
 from flask import render_template, url_for, request, redirect
+from ..requests import get_random_quotes
 
 
 
 @main.route('/')
 def index():
-
-
-  return render_template('index.html')
+  rand_quote = get_random_quotes()
+  title = "Welcome to my personal blogs | Be Inspired"
+  return render_template('index.html', quote=rand_quote, title=title)
