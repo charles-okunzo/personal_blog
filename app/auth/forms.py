71569531c64c2c260dcc81fda20c7dd8788a2db5
charles okunzo <okunzopeter01@gmail.com>
2022-05-15 16:37:34 +0300

    feature --add signin form class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,10 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SelectField, SubmitField, PasswordField
-from wtforms.validators import Email, EqualTo, DataRequired
-
+from wtforms import StringField, TextAreaField, SelectField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import Email, EqualTo, DataRequired, ValidationError
+from ..models import User
 
 class SignupForm(FlaskForm):
   username = StringField('Enter username:', validators=[DataRequired()])
   email = StringField('Enter email', validators=[DataRequired(), Email()])
   password = PasswordField('Enter password', validators=[DataRequired(), EqualTo('confirm_password', 'Passwords do not match!')])
-  submit = SubmitField('Signup')
+  submit = SubmitField('Sign Up')
+
+
+  def validate_username(self, data_field):
+    if User.query.filter_by(username = data_field.data).first():
+      raise ValidationError('Username already exists')
+
+
+  def validate_email(self, data_field):
+    if User.query.filter_by(email=data_field.data).first():
+      raise ValidationError('This email has already been used')
+
+
+
+class LoginForm(FlaskForm):
+  email = StringField('Enter your email:', validators=[DataRequired(), Email()])
+  password = PasswordField('Enter your password:', validators=[DataRequired()])
+  remember = BooleanField('Remember me')
+  submit  = SubmitField('Sign In')
