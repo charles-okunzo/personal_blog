08225f76a2d0b307b46561a65f282fa8d5137862
charles okunzo <okunzopeter01@gmail.com>
2022-05-16 13:18:50 +0300

    deployment --add configurations for postgresql

--- config.py
+++ config.py
@@ -13,6 +13,10 @@ class Config:
 
 class ProdConfig(Config):
   SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL')
+  if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+    SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
+
+
 
 
 
