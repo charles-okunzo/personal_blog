9af3d9464f2cb6706f4333e99f08cce926a52be0
charles okunzo <okunzopeter01@gmail.com>
2022-05-16 11:26:26 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 
 from app import create_app
-from flask_script import Manager,Server
+# from flask_script import Manager,Server
 from app import db
 from app.models import User
 from flask_migrate import Migrate
@@ -9,19 +9,19 @@ from flask_migrate import Migrate
 
 
 app = create_app('production')
-manager = Manager(app)
+# manager = Manager(app)
 migrate = Migrate(app,db)
 
-manager.add_command('server', Server)
+# manager.add_command('server', Server)
 
 
 
-@manager.shell
-def create_shell_context():
-  return dict(app=app, db=db, User=User)
+# @manager.shell
+# def create_shell_context():
+#   return dict(app=app, db=db, User=User)
 
 
 
 
 if __name__ == '__main__':
-  manager.run()
+  app.run()
